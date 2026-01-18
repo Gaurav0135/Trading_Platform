00b35d9227c2ebe86f4671038e3af9ae90056723
Gaurav0135 <gauravpatel220931@gmail.com>
2026-01-18 17:41:22 +0530

    Initial commit: Zerodha (frontend, backend, dashboard)

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+
+import Navbar from 'landing_page/Navbar';
+import Footer from 'landing_page/Footer';
+
+function PricingPage() {
+    return (
+        <>  
+            <Hero />
+            <Brokerage />
+        </>
+      );
+}
+
+export default PricingPage;
